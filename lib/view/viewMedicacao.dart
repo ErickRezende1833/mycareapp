759c4aced3bd5ec89d1a887a0b49210594a713fe
Erick <persona0rcc@gmail.com>
2025-11-23 13:33:23 -0300

    feat: add e get medicacao funcionando

--- lib/view/viewMedicacao.dart
+++ lib/view/viewMedicacao.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/medicacao.dart'; // Usando medicacao.dart
+import 'package:flutter_application_1/view/viewAddMedicacao.dart';
+import 'package:flutter_application_1/models/historicoAcao.dart';
 
-class ViewMedicacao extends StatelessWidget {
+class ViewMedicacao extends StatefulWidget {
   const ViewMedicacao({super.key});
+
+  @override
+  State<ViewMedicacao> createState() => _ViewMedicacaoState();
+}
+
+class _ViewMedicacaoState extends State<ViewMedicacao> {
+  final List<Medicacao> _minhasMedicacoes = [
+    Medicacao(
+      nome: "Diazepan",
+      dose: "1g",
+      horario: const TimeOfDay(hour: 8, minute: 0),
+    ),
+    Medicacao(
+      nome: "Diazepan",
+      dose: "1g",
+      horario: const TimeOfDay(hour: 16, minute: 0),
+    ),
+  ];
+
+  final List<HistoricoAcao> _historicoAcoes = [];
+
+  void _navigateToAddMedicacao() async {
+    final novaMedicacao = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ViewAddMedicacao()),
+    );
+
+    if (novaMedicacao != null && novaMedicacao is Medicacao) {
+      setState(() {
+        _minhasMedicacoes.add(novaMedicacao);
+      });
+    }
+  }
+
+  void _registrarAcao(Medicacao medicacao, bool tomada) {
+    setState(() {
+      final novaAcao = HistoricoAcao(
+        nomeMedicamento: medicacao.nome,
+        horarioPrevisto: medicacao.horario, 
+        tomada: tomada,
+        registro: DateTime.now(),
+      );
+      
+      _historicoAcoes.insert(0, novaAcao);
+    });
+  }
+
+  void _removerMedicacao(Medicacao medicacao) {
+    setState(() {
+      _minhasMedicacoes.remove(medicacao);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,41 +71,51 @@ class ViewMedicacao extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "Meus Medicamentos",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 5),
 
-            SizedBox(height: 5),
-
-            MedicamentoCard(
-              hora: "08:00",
-              nomeMedicamento: "Diazepan",
-              dose: "1g",
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _minhasMedicacoes.length,
+                itemBuilder: (context, index) {
+                  final med = _minhasMedicacoes[index];
+                  return MedicamentoCard(
+                    medicacao: med,
+                    onAction: _registrarAcao,   
+                    onRemove: _removerMedicacao, 
+                  );
+                },
+              ),
             ),
-            MedicamentoCard(
-              hora: "16:00",
-              nomeMedicamento: "Diazepan",
-              dose: "1g",
+            
+            const SizedBox(height: 15),
+            const Text(
+              "Histórico",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            
+            SizedBox(
+              height: 150,
+              child: ListView.builder(
+                itemCount: _historicoAcoes.length,
+                itemBuilder: (context, index) {
+                  final acao = _historicoAcoes[index];
+                  return HistoricoCard(acao: acao);
+                },
+              ),
             ),
 
-            SizedBox(height: 5),
-
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                CustomIconButton(
-                  onPressed: () {},
-                  icon: Icons.menu,
-                  color: Colors.blue,
-                ),
-                RevCustomElevatedButton(
-                  onPressed: () {},
-                  icon: Icons.delete_forever_outlined,
-                  label: "Remover",
-                ),
                 CustomElevatedButton(
-                  onPressed: () {},
+                  onPressed: _navigateToAddMedicacao,
                   icon: Icons.add,
                   label: "Adicionar",
                 ),
@@ -59,32 +124,35 @@ class ViewMedicacao extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: CustomBottomNavigationBar(),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 }
 
-//card de remedio
+
+//CARD DE MEDICAMENTO
 
 class MedicamentoCard extends StatelessWidget {
-  final String hora;
-  final String nomeMedicamento;
-  final String dose;
+  final Medicacao medicacao;
+  final Function(Medicacao, bool) onAction; 
+  final Function(Medicacao) onRemove;
 
   const MedicamentoCard({
     super.key,
-    required this.hora,
-    required this.nomeMedicamento,
-    required this.dose,
+    required this.medicacao,
+    required this.onAction,
+    required this.onRemove,
   });
 
   @override
   Widget build(BuildContext context) {
+    const Color cardColor = Color.fromARGB(255, 230, 230, 230); 
+
     return Container(
       padding: const EdgeInsets.all(12),
       margin: const EdgeInsets.symmetric(vertical: 5),
       decoration: BoxDecoration(
-        color: Colors.grey[300],
+        color: cardColor,
         borderRadius: BorderRadius.circular(12),
       ),
       child: IntrinsicHeight(
@@ -100,54 +168,126 @@ class MedicamentoCard extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Text(
-                hora,
+                medicacao.horaFormatada,
                 style: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             const SizedBox(width: 12),
-            // Informações de medicamento
+        
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    nomeMedicamento,
+                    medicacao.nome,
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
                     ),
                   ),
                   Text(
-                    dose,
+                    medicacao.dose,
                     style: const TextStyle(fontSize: 12, color: Colors.black54),
                   ),
                 ],
               ),
             ),
+            
             Column(
               children: [
-                Text("Tomei?"),
-                SizedBox(height: 5),
+                const Text("Tomei?"),
+                const SizedBox(height: 5),
                 Row(
                   children: [
                     CustomIconButton(
-                      onPressed: () {},
+                      onPressed: () => onAction(medicacao, true),
                       icon: Icons.check,
                       color: Colors.green,
                     ),
-                    SizedBox(width: 8),
+                    const SizedBox(width: 8),
                     CustomIconButton(
-                      onPressed: () {},
+                      onPressed: () => onAction(medicacao, false),
                       icon: Icons.close,
                       color: Colors.red,
                     ),
+                    const SizedBox(width: 20),
+                 
                   ],
                 ),
               ],
             ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomIconButton(
+                      onPressed: () => onRemove(medicacao),
+                      icon: Icons.delete_forever_outlined,
+                      color: Colors.blueGrey,
+                    )
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+// CARD DE HISTÓRICO
+
+
+class HistoricoCard extends StatelessWidget {
+  final HistoricoAcao acao;
+
+  const HistoricoCard({super.key, required this.acao});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color cardColor = acao.tomada ? Colors.green[200]! : Colors.red[200]!;
+    final String statusText = acao.tomada ? "Tomou" : "Não Tomou";
+
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(vertical: 2),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                acao.nomeMedicamento,
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+              Text(
+                "Previsto: ${acao.horaFormatadaPrevista}",
+                style: const TextStyle(fontSize: 10, color: Colors.black54),
+              ),
+            ],
+          ),
+          
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                statusText,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: acao.tomada ? Colors.green[800] : Colors.red[800]),
+              ),
+              Text(
+                "${acao.dataHoraFormatadaRegistro}",
+                style: const TextStyle(fontSize: 10, color: Colors.black54),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
